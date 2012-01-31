352dcb058c61d7e65cd5c0707b948c3f9aa510c0
Corvin89 <corvin8989@gmail.com>
2012-01-31 14:40:28 +0200

    Добавил post-thumbnails

--- app/wp-content/themes/student/footer.php
+++ app/wp-content/themes/student/footer.php
@@ -24,5 +24,6 @@
         </div>
     </div>
 </div>
+<?php wp_footer(); ?>
 </body>
 </html>
--- app/wp-content/themes/student/functions.php
+++ app/wp-content/themes/student/functions.php
@@ -1,1 +1,6 @@
 <?php
+add_theme_support('post-thumbnails');
+
+if ( function_exists('add_theme_support') ) {
+    add_theme_support('post-thumbnails');
+}
