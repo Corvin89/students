92d5d3130710ca8f752932ba813255aa9d6bfeb5
Corvin89 <corvin8989@gmail.com>
2012-01-31 10:57:17 +0200

    update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!sh
 #git stash
 #git pull origin master
-mysql -ustudent -padmin -e "drop database student;"
-mysql -ustudent -padmin -e "create database student;"
-mysql -ustudent -padmin student < backup/database.sql
-mysql -ustudent -padmin -e "UPDATE wp_options SET option_value = 'http://student.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" student
+mysql -uadmin -padmin -e "drop database student;"
+mysql -uadmin -padmin -e "create database student;"
+mysql -uadmin -padmin student < backup/database.sql
+mysql -uadmin -padmin -e "UPDATE wp_options SET option_value = 'http://student.dev/' WHERE option_name = 'home' OR option_name = 'siteurl';" student
 #git stash pop
 
