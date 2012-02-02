a85ab231b48b30330d2cca492988474580dff375
Corvin89 <corvin8989@gmail.com>
2012-02-02 20:12:49 +0200

    Social networks in sidebar-bottom and footer #9

---
+++ app/wp-content/themes/student/functions/admin-menu.php
@@ -0,0 +1,56 @@
+<?php
+
+// Создать пользовательское меню
+add_action('admin_menu', 'register_my_custom_submenu_page');
+
+
+function register_my_custom_submenu_page() {
+    add_submenu_page( 'themes.php', 'Настройка темы', 'Настройка темы', 'manage_options', 'my-custom-submenu-page', 'my_custom_submenu_page_callback' );
+}
+
+function my_custom_submenu_page_callback() {
+    if(isset($_POST['settings'])) {
+        unset($_POST['settings']);
+//        $_POST['working_days'] = join(',', $_POST['working_days']);
+        foreach($_POST as $option=>$value) {
+            update_option($option, $value);
+        }
+    }
+    ?>
+<div class="wrap">
+    <h2>Настройка темы</h2>
+
+    <form method="post" action="themes.php?page=my-custom-submenu-page">
+        <input type="hidden" name="settings">
+        <table class="form-table">
+
+            <tr valign="top">
+                <th scope="row">vk.com</th>
+                <td><input style="width:400px; height:25px;" type="text" name="student_vk" value="<?php echo get_option('student_vk', 'http://vk.com');?>" />
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">mail.ru</th>
+                <td><input style="width:400px; height:25px;" type="text" name="student_mail" value="<?php echo get_option('student_mail', 'http://mail.ru');?>" />
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">twitter</th>
+                <td><input style="width:400px; height:25px;" type="text" name="student_twitter" value="<?php echo get_option('student_twitter', 'http://twitter.com/');?>" />
+                </td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">google +</th>
+                <td><input style="width:400px; height:25px;" type="text" name="student_google" value="<?php echo get_option('student_google', 'https://plus.google.com');?>" />
+                </td>
+            </tr>
+        </table>
+
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>"/>
+        </p>
+    </form>
+</div>
+<?php
+
+}
--- app/wp-content/themes/student/sidebar-bottom.php
+++ app/wp-content/themes/student/sidebar-bottom.php
@@ -1,7 +1,7 @@
 <div class="top-green-big">Мы в сети</div>
 <ul class="soc-icon">
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon1.gif" alt="" title="" /></a></li>
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon2.gif" alt="" title="" /></a></li>
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon3.gif" alt="" title="" /></a></li>
-    <li><a href="#"><img src="<?php bloginfo('template_directory'); ?>/img/icon4.gif" alt="" title="" /></a></li>
+    <li><a href="<?php echo get_option('vkontakte');?>"><img src="<?php bloginfo('template_directory'); ?>/img/icon1.gif" alt="" title="" /></a></li>
+    <li><a href="<?php echo get_option('mail');?>"><img src="<?php bloginfo('template_directory'); ?>/img/icon2.gif" alt="" title="" /></a></li>
+    <li><a href="<?php echo get_option('twitter');?>"><img src="<?php bloginfo('template_directory'); ?>/img/icon3.gif" alt="" title="" /></a></li>
+    <li><a href="<?php echo get_option('google');?>"><img src="<?php bloginfo('template_directory'); ?>/img/icon4.gif" alt="" title="" /></a></li>
 </ul>
