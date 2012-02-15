576503682ece44681c91494d61fb201bb85f63c9
Corvin89 <corvin8989@gmail.com>
2012-02-15 19:54:57 +0200

    Подправил вывод подписки

--- app/wp-content/themes/student/sidebar-top.php
+++ app/wp-content/themes/student/sidebar-top.php
@@ -1,4 +1,3 @@
-<p>Join 150,000 others: Sign up for our daily or weekly newsletters so you won't miss the latest and greatest</p>
 <?php if ( is_active_sidebar( 'subscribe-sidebar' ) ) { ?>
 <?php if ( !dynamic_sidebar('subscribe-sidebar') ) : ?>
     <?php endif; ?>
